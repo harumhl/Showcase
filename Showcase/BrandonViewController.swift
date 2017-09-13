c0fa3a69f754ab96c43ccca1adf27448551d1705
ellisbrandon20 <ellis.brandon2017@gmail.com>
2017-09-13 16:16:08 -0500

    new view controller

---
+++ Showcase/BrandonViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BrandonViewController.swift
+//  Showcase
+//
+//  Created by ellisbrandon20 on 9/13/17.
+//  Copyright © 2017 TamuCpse482. All rights reserved.
+//
+
+import UIKit
+
+class BrandonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
