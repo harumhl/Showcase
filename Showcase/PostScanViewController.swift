25fdf9e5bf074db2dbebe48f1ca62ac6db564d17
Brian Ta <Froob@tamu.edu>
2017-09-18 16:36:15 -0500

    Initial barcode work

---
+++ Showcase/PostScanViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PostScanViewController.swift
+//  Showcase
+//
+//  Created by Brian Ta on 9/18/17.
+//  Copyright © 2017 TamuCpse482. All rights reserved.
+//
+
+import UIKit
+
+class PostScanViewController: UIViewController {
+
+    @IBOutlet weak var barcodeField: UILabel!
+    var theBarcode: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        barcodeField.text = theBarcode
+        barcodeField .adjustsFontSizeToFitWidth = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Showcase/ScannerViewController.swift
+++ Showcase/ScannerViewController.swift
@@ -5,31 +5,111 @@
 //  Created by ellisbrandon20 on 9/18/17.
 //  Copyright © 2017 TamuCpse482. All rights reserved.
 //
+// Source code inspired from: https://www.hackingwithswift.com/example-code/media/how-to-scan-a-barcode
 
+import AVFoundation
 import UIKit
 
-class ScannerViewController: UIViewController {
-
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var scannedBarcode: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.black
+        captureSession = AVCaptureSession()
+
+        let videoCaptureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed();
+            return;
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypePDF417Code]
+        }
+        else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession);
+        previewLayer.frame = view.layer.bounds;
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        view.layer.addSublayer(previewLayer);
+
+        captureSession.startRunning();
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning();
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning();
+        }
+    }
+
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        captureSession.stopRunning()
+
+        if let metadataObject = metadataObjects.first {
+            let readableObject = metadataObject as! AVMetadataMachineReadableCodeObject;
+
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: readableObject.stringValue);
+        }
+
+        dismiss(animated: false)
+    }
+
+    func found(code: String) {
+        print("Finished scanning code: " + code)
+        scannedBarcode = code
+        performSegue(withIdentifier: "ScannerToSearch", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let postScanVC: PostScanViewController = segue.destination as! PostScanViewController
+        postScanVC.theBarcode = scannedBarcode
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override var prefersStatusBarHidden: Bool {
+        return false
     }
-    */
 
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
