f18a6cd805f8430fea33fbaefd76462865d6f6a5
haru.mhl <haru.mhl@TAMU.EDU@scc-100-164.local>
2017-10-02 17:13:31 -0500

    gitignore testing

---
+++ Showcase/Keys.swift
@@ -0,0 +1,15 @@
+//
+//  Keys.swift
+//  Showcase
+//
+//  Created by ellisbrandon20 on 10/2/17.
+//  Copyright © 2017 TamuCpse482. All rights reserved.
+//
+
+import Foundation
+
+
+let accessKeyId = "aaa"
+let associateTag = "showcasetamu4-20"
+let accessSecretKey = ""
+let goodReadsKey = ""
