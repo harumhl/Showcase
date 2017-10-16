27745e16bea02a9bcce8e47bbcf4d02d0b2b657a
ellisbrandon20 <ellisbrandon20@TAMU.EDU@scc-100-164.local>
2017-10-16 16:55:07 -0500

    able to read reviews from amazon

--- Showcase/PostScanViewController.swift
+++ Showcase/PostScanViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import Cosmos
+import SwiftSoup
 
 
 
@@ -46,6 +47,7 @@ class PostScanViewController: UIViewController{
         cosmosView.settings.filledBorderColor = UIColor.black
         
         // Updating the Display
+        getReviewsFromReviewURL()
         displayBookInfo()
         
         addDataToDB()
@@ -74,6 +76,163 @@ class PostScanViewController: UIViewController{
         // https://github.com/evgenyneu/Cosmos
         cosmosView.rating = bookData.rating
         cosmosView.text = String(format:"%.2f", bookData.rating)
+    }
+    
+    
+    func getReviewsFromReviewURL() {
+        
+        
+            let theURL = bookData.reviewURL
+            print("Review URL: " + theURL)
+            
+            // Skip if the book has no reviews
+            if (theURL == "Reviews Not Available") {
+                print("Reviews Not Available")
+            }
+            
+            // Check the validity of the URL ("guard" checks it)
+            guard let url = URL(string: theURL) else {
+                print("Error: cannot create URL")
+                return
+            }
+            
+            // Get the HTML source from the URL
+            var myHTMLString = ""
+            do {
+                myHTMLString = try String(contentsOf: url)
+                //print("HTML : \(myHTMLString)")
+            } catch let error as NSError {
+                print("Error: \(error)")
+            }
+            
+            // Use Swift Soup to parse the HTML source
+            do {
+                let doc = try SwiftSoup.parse(myHTMLString)
+                
+                // get the All Reviews Link
+                let reviewElem: Elements = try doc.getElementsByClass("asinReviewsSummary")
+                let reviewTag: Elements = try reviewElem.select("a")
+                let reviewLink: String = try! reviewTag.attr("href")
+                print("reviewLink: " + reviewLink)
+                
+                
+                // get the HTML code from the All Reviews URL
+                guard let reviewURL = URL(string: reviewLink) else {
+                    print("Error: cannot create URL")
+                    return
+                }
+                var reviewURlHTMLString = ""
+                do {
+                    reviewURlHTMLString = try String(contentsOf: reviewURL)
+                } catch let error as NSError {
+                    print("Error: \(error)")
+                }
+                
+                // Parse the HTML
+                let reviewDoc = try SwiftSoup.parse(reviewURlHTMLString)
+                
+                
+                // "a-section review" gives us the entire review data
+                
+                // $(".review .a-link-normal").title; gives you the rating ex: "5.0 out of 5 stars"
+                    // then we can check for the first part of that string to assign to a Double variable
+                
+                
+                // get the array of "review-text" elements
+                let elems: Elements = try reviewDoc.getElementsByClass("review-text")
+                for review: Element in elems.array(){
+                    let reviewText = try review.text()
+                    print("Review: " + reviewText)
+                }
+                
+                
+                
+
+//                let link: Elements = try doc.select("a[href]")
+//                let linkHref: String = try! link.attr("href");
+                
+//                let elems: Elements = try doc.select("reviewText")
+//                for review: Element in elems.array(){
+//                    let reviewStr: String =  try review.text()
+//                    print("reviewSTR: " + reviewStr)
+//                }
+                
+                
+                
+                
+                
+//                let docBody = doc.body()
+//                //print("Doc Body: ")
+//                //print(docBody)
+//                //print("\n\n")
+//
+//                let elem = try doc.getElementsByClass("crIframeReviewList").get(0)
+//                let table = try elem.getElementsByTag("table").get(0)
+//                let tbody = try table.getElementsByTag("tbody").get(0)
+//                let tr = try tbody.getElementsByTag("tr").get(0)
+//                let td = try tr.getElementsByTag("td").get(0)
+//                let divs = try td.getElementsByTag("div")
+//
+//                print("Doc elem: ")
+//                for div in divs { // each review
+//                    //print("hmm?????")
+//                    //print(div)
+//                    //print(try div.text())
+//
+//                    if (try div.getElementsByTag("b").array().count > 0) {
+//                        print(try div.getElementsByTag("b").get(0))
+//                    }
+//
+//                    if (try div.getElementsByTag("div").array().count > 0) {
+//                        let div_ = try div.getElementsByTag("div").get(0)
+//                        let div__ = try div_.getElementsByTag("div")
+//                        //print(div_)
+//
+//                        if (div__.array().count > 1) {
+//                            let div1 = div__.get(1)
+//                            let div1b = try div1.getElementsByTag("b")
+//                            if (div1b.array().count > 0) {
+//                                let title = try div1b.array()[0].text() // NOT ALWAYS!!!!
+//                                print("Title:: " + title)
+//                            }
+//                        }
+//                    }
+//                }
+                /*
+                 //print(try divs.array()[0].text() + "\n\n\n")
+                 let aaa = try divs.get(0).getElementsByTag("div").get(0)
+                 let bbb = try aaa.getElementsByTag("div").array()[0]
+                 let ccc = try bbb.getElementsByTag("div")
+                 print(bbb)
+                 let title = try ccc.get(1).getElementsByTag("b")
+                 print(try title.text())
+                 //let author = try aaa.get(2).getElementsbyTag("div").get
+                 */
+                
+            } catch {
+                print("error")
+            }
+            
+//            // Start URL session
+//            let urlRequest = URLRequest(url: url)
+//            let session = URLSession.shared
+//
+//            // Unpack the returned XML data
+//            let task = session.dataTask(with: urlRequest) {
+//                (data, response, error) in
+//                // check for any errors
+//                guard error == nil else {
+//                    print("error calling GET on /todos/1")
+//                    print(error!)
+//                    return
+//                }
+//                // make sure we got data
+//                guard let responseData = data else {
+//                    print("Error: did not receive data")
+//                    return
+//                }
+//            }
+//            task.resume()
     }
 
 //****************************************** Database Functions **********************************************
